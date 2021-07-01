07e30672257f30c66f7c00464f98848f710f1b28
Joe Pentland <joe.pentland@gmail.com>
2021-07-01 17:37:54 +0200

    tcm.py: Use config file for basic parameters, ask for credentials if dont exist

--- README.txt
+++ README.txt
@@ -2,14 +2,14 @@ Installing Python, and the pyautogui library
 
 1. Download and install: https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe
 2. Open run dialog with Win+R, type cmd, and press ok
-3. In the command prompt, type the command "py -m pip install pyautogui" and press return
+3. In the command prompt, type the command "py -m pip install pyautogui appdirs toml" and press return
 4. Wait for the installation
 4. Close the command prompt
 
 Setup
 1. Request an oauth code with this link https://twitchapps.com/tmi/ (don't leak this on stream)
-2. Edit tcm.py, and set the correct username and oauth variables
-3. To start twitch controlling mouse, double click tcm.py
+2. To start twitch controlling mouse, double click tcm.py
+3. If this is the first time running the program, you will be asked for your twitch username and oauth. You can paste by just right-clicking in the command prompt. Press enter to continue.
 4. To stop, close the command prompt that opens, or use command !quittcm
 5. If something doesnt work, screenshot the command prompt and send to sharperguy
 
@@ -39,3 +39,4 @@ Note:
 * !box will drag a box with a given size, starting from the current mouse position
 * !quittcm only works for the streamer
 * The commands !mouseup, !mousedown, !boxdownleft, !boxupright etc can also take an optional distance.
+* The config file is located in: C:\Users\<username>\AppData\Local\TwitchPlaysRTS\TwitchPlaysRTS\config.toml, which stores credentials and other information.
--- tcm.py
+++ tcm.py
@@ -1,30 +1,37 @@
-#!/usr/bin/env python
-
-# PLEASE SET YOUR INFORMATION HERE
-username = "username"
-oauth = "oauth:xxxxxxxxxx"
-
-# DONT CHANGE ANYTHING BELOW THIS LINE
-
+#!/usr/bin/env python3
 import pyautogui as pg
 import time
 import socket
 import re
 import sys
+import os
 from queue import Queue
 import threading
+from appdirs import *
+import toml
+import pathlib
+
+appname = "TwitchPlaysRTS"
+appauthor = "TwitchPlaysRTS"
+config_dir = user_data_dir(appname, appauthor)
+config_file_path = os.path.join(config_dir, "config.toml")
 
 pg.PAUSE = 0
 pg.FAILSAFE=False
-ircDomain = "irc.chat.twitch.tv"
-ircPort = 6667
-timeout = 2
-defaultDistance = 10
 
-PING_MSG = "PING :tmi.twitch.tv"
-PONG_MSG = "PONG :tmi.twitch.tv"
+defaultConfig = {
+        "execution" : {
+            "timeout" : 2,
+            "defaultDistance" : 10
+        },
+        "irc" : {
+            "domain" : "irc.chat.twitch.tv",
+            "port" : 6667,
+            "PING_MSG" : "PING :tmi.twitch.tv",
+            "PONG_MSG" : "PONG :tmi.twitch.tv"
+        }
+}
 
-reMessage = re.compile("([^\s]+)!.* PRIVMSG #" + username + " :(.*)")
 reMoveMouse = re.compile("!(?:movemouse|mouse) ([0-9\.]+) ([0-9\.]+)")
 reBox = re.compile("!box (-?[0-9\.]+) (-?[0-9\.]+)")
 reClick = re.compile("!click")
@@ -61,6 +68,40 @@ commandQueue = Queue()
 
 screenWidth, screenHeight = pg.size()
 
+# Get contents of config file or defaultconfig if doesnt exist
+def loadConfig():
+    try:
+        with open(config_file_path) as configFile:
+            content = configFile.read()
+            return toml.loads(content)
+    except FileNotFoundError:
+        return defaultConfig
+
+# Write config to disk
+def writeConfig(config):
+    pathlib.Path(config_dir).mkdir(parents=True, exist_ok=True)
+    with open(config_file_path, "w") as configFile:
+        toml.dump(config, configFile)
+
+# CLI: display error message, press any key to exit
+def errorOut(msg):
+    print("ERROR: %s" % msg)
+    input("Press RETURN to exit\n")
+    sys.exit(1)
+
+# CLI: Get username and oauth values if not set in config
+def getCredentials(config):
+    if "credentials" not in config:
+        config["credentials"] = {}
+
+    if "username" not in config["credentials"]:
+        config["credentials"]["username"] = input("Please enter your stream username: ")
+
+    if "oauth" not in config["credentials"]:
+        config["credentials"]["oauth"] = input("Please enter your stream oauth: ")
+
+    return config
+
 def percentageToPixel(x, y):
     return screenWidth * (x/100), screenHeight * (y/100)
 
@@ -86,12 +127,12 @@ def send(server, string):
     server.send(bytes(string + '\r\n', 'utf-8'))
     
 def runIrc():
-    connection_data = (ircDomain, ircPort)
+    connection_data = (config["irc"]["domain"], config["irc"]["port"])
     server = socket.socket()
     server.connect(connection_data)
-    send(server, 'PASS ' + oauth)
-    send(server, 'NICK ' + username)
-    send(server, 'JOIN #' + username)
+    send(server, 'PASS ' + config["credentials"]["oauth"])
+    send(server, 'NICK ' + config["credentials"]["username"])
+    send(server, 'JOIN #' + config["credentials"]["username"])
     while(True):
         try:
             message = server.recv(2048).decode('utf-8')
@@ -100,10 +141,10 @@ def runIrc():
                 server.close()
                 exit(0)
 
-            elif (PING_MSG[:4] == message[:4]):
+            elif (config["irc"]["PING_MSG"][:4] == message[:4]):
                 print("twitch pinged")
                 print(repr(message))
-                send(server, PONG_MSG)
+                send(server, config["irc"]["PONG_MSG"])
             
             else:
                 processChatMessage(message)
@@ -123,13 +164,14 @@ def processCommandQueue():
     while(True):
         try:
             epoch, sender, command = commandQueue.get()
-            if epoch + timeout >= time.time():
-                processCommand(username, command)
+            if epoch + config["execution"]["timeout"] >= time.time():
+                processCommand(config["credentials"]["username"], command)
         except:
             pass
 
 def processCommand(sender, message):
     print("\"" + message + "\"")
+    defaultDistance = config["execution"]["defaultDistance"]
     match = reMoveMouse.match(message)
     if match != None:
         x = float(match.group(1))
@@ -153,7 +195,7 @@ def processCommand(sender, message):
     if match != None:
         pg.doubleClick()
     match = reQuit.match(message)
-    if match != None and sender == username:
+    if match != None and sender == config["credentials"]["username"]:
         sys.exit(0)
 
     match = reMouseUpLeftCoord.match(message)
@@ -265,6 +307,13 @@ def processCommand(sender, message):
     if match != None:
         box(defaultDistance, defaultDistance)
         return
+
+config = loadConfig()
+config = getCredentials(config)
+writeConfig(config)
+
+# TODO: Move this somewhere sensible
+reMessage = re.compile("([^\s]+)!.* PRIVMSG #" + config["credentials"]["username"] + " :(.*)")
 
 ircThread = threading.Thread(target = runIrc, daemon = True)
 ircThread.start()
