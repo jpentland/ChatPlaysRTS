f25da524db67eaa0650bbb6262c8da60eae777c0
Joe Pentland <joe.pentland@gmail.com>
2021-07-08 12:31:41 +0200

    doc: New directory containing all text files except README.txt and LICENSE.txt

--- START.bat
+++ START.bat
@@ -1,4 +1,4 @@
-for /f "delims== tokens=1,2" %%G in (DEPS.txt) do (
+for /f "delims== tokens=1,2" %%G in (doc\DEPS.txt) do (
 	py -m pip install %%G
 )
 py src/tprts.py
--- unix.sh
+++ unix.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for a in $(cat DEPS.txt); do
+for a in $(cat doc/DEPS.txt); do
 	python3 -m pip install $a
 done
 python3 src/tprts.py
