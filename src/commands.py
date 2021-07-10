83d1b5a428d2d114b8c07e75315664486c486e75
Joe Pentland <joe.pentland@gmail.com>
2021-07-10 12:57:43 +0200

    commands: Refactor and produce more meaningful exceptions/log messages

--- src/commands.py
+++ src/commands.py
@@ -6,6 +6,10 @@ class RegexError(Exception):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+class TomlError(Exception):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
 class Commands():
     def __init__(self, config, log):
         self.config = config
@@ -20,59 +24,95 @@ class Commands():
                 self.config.user_commands_file
         )
         self.loadCommands()
+
+    # Load commands from usercommands and default commands
+    def loadCommands(self):
+        try:
+            self.migrateOldCommands()
+        except Exception as e:
+            self.log.log("Error occurred while migrating old %s, you may want to delete it" % self.config.commands_file)
+            self.log.log_exception(e)
+
+        self.loadDefaultCommands()
+
+        try:
+            self.loadUserCommands()
+        except FileNotFoundError as e:
+            self.log.log("%s not found" % self.config.user_commands_file)
+
         self.compile()
 
-    def loadCommands(self):
-        if os.path.isfile(os.path.join(self.config.config_dir, self.config.commands_file)):
-            self.migrateOldCommands()
+        self.log.log("%d total commands compiled" % len(self.data))
 
+    # Load default commands
+    def loadDefaultCommands(self):
         with open(self.commands_file_path) as commandsFile:
-                content = commandsFile.read()
+            content = commandsFile.read()
+
+            try:
                 defaultCommands = toml.loads(content)["command"]
-                self.data += defaultCommands
-        try:
-            with open(self.user_commands_file_path) as commandsFile:
-                content = commandsFile.read()
+            except toml.decoder.TomlDecodeError as e:
+                self.log.log("Failed to read %s" % self.commands_file_path)
+                raise TomlError(e)
+
+            self.data += defaultCommands
+
+    # Load user commands
+    def loadUserCommands(self):
+        with open(self.user_commands_file_path) as commandsFile:
+            content = commandsFile.read()
+            try:
                 userCommands = toml.loads(content)["command"]
-                self.log.log("Loaded %d custom commands from %s" \
-                        % (len(userCommands), self.config.user_commands_file))
-                self.data += userCommands
-        except FileNotFoundError:
-            self.log.log("No user commands file found")
+            except toml.decoder.TomlDecodeError as e:
+                self.log.log("Failed to read %s" % self.config.user_commands_file)
+                raise TomlError(e)
+
+            self.log.log("Loaded %d custom commands from %s" \
+                    % (len(userCommands), self.config.user_commands_file))
+
+            self.data += userCommands
 
     # Migrate old commands file (pre v0.8)
-    def migrateOldCommands():
-        defaultCommands = []
-        oldCommands = []
-        newCommands = []
+    def migrateOldCommands(self):
         old_commands_file_path = os.path.join(
                 self.config.config_dir,
                 self.config.commands_file
         )
+        if os.path.isfile(old_commands_file_path):
+            defaultCommands = []
+            oldCommands = []
+            newCommands = []
 
-        with open(self.commands_file_path) as commandsFile:
-                content = commandsFile.read()
-                defaultCommands += toml.loads(content)["command"]
+            with open(self.commands_file_path) as commandsFile:
+                    content = commandsFile.read()
+                    defaultCommands += toml.loads(content)["command"]
 
-        with open(old_commands_file_path) as commandsFile:
-                content = commandsFile.read()
-                oldCommands += toml.loads(content)["command"]
-
-        # Import any non-default commands in commands.toml to user_commands.toml
-        for command in oldCommands:
             try:
-                next(filter(lambda x : x["regex"] == command["regex"], defaultCommands))
-            except StopIteration:
-                newCommands.append(command)
+                with open(self.user_commands_file_path) as commandsFile:
+                        content = commandsFile.read()
+                        newCommands += toml.loads(content)["command"]
+            except FileNotFoundError:
+                pass
 
-        if len(newCommands) > 0:
-            with open(self.user_commands_file_path, "w") as commandsFile:
-                toml.dump({"command" : newCommands}, commandsFile)
+            with open(old_commands_file_path) as commandsFile:
+                    content = commandsFile.read()
+                    oldCommands += toml.loads(content)["command"]
 
-            self.log.log("Migrated %d commands from old commands.toml to new user_commands.toml" % len(newCommands))
+            # Import any non-default commands in commands.toml to user_commands.toml
+            for command in oldCommands:
+                try:
+                    next(filter(lambda x : x["regex"] == command["regex"], defaultCommands))
+                except StopIteration:
+                    newCommands.append(command)
 
-        self.log.log("Deleting old " + commands_file)
-        os.remove(self.old_commands_file_path)
+            if len(newCommands) > 0:
+                with open(self.user_commands_file_path, "w") as commandsFile:
+                    toml.dump({"command" : newCommands}, commandsFile)
+
+                self.log.log("Migrated %d commands from old commands.toml to new user_commands.toml" % len(newCommands))
+
+            self.log.log("Deleting old " + self.config.commands_file)
+            os.remove(old_commands_file_path)
 
     # Compile regex for all commands before main program start
     def compile(self):
