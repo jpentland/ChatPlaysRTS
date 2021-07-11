b116692b1d4e630f479bb75c0d76428473d8a47e
Joe Pentland <joe.pentland@gmail.com>
2021-07-11 17:37:46 +0200

    contoller.py: Thread to control IRC connection and run execution

---
+++ src/controller.py
@@ -0,0 +1,67 @@
+import sys
+import threading
+import time
+from twitchirc import TwitchIrc
+from execution import Execution
+from config import Config
+from log import Log
+from commands import Commands
+from error import *
+
+class Controller(threading.Thread):
+    def __init__(self, config, log, onConnect, onDisconnect):
+        threading.Thread.__init__(self, daemon = True)
+        self.config = config
+        self.log = log
+        self.onConnect = onConnect
+        self.onDisconnect = onDisconnect
+
+    def errorOut(self, message):
+        self.log.log(message)
+
+    def run(self):
+        self.irc = TwitchIrc(self.config, self.log)
+
+        try:
+            self.irc.connect(5)
+        except AuthenticationError:
+            self.errorOut("Invalid username or oauth")
+            return
+        except ConnectionFailedError:
+            self.errorOut("Failed to connect to Twitch")
+            return
+        except Exception:
+            self.log.log_exception(e)
+            self.errorOut("Failed to connect to Twitch")
+            return
+
+        try:
+            self.commands = Commands(self.config, self.log)
+        except (RegexError, TomlError, FileNotFoundError) as e:
+            self.log.log_exception(e)
+            self.errorOut("Failed to load commands")
+            return
+
+        self.execution = Execution(self.config, self.commands, self.irc, self.log)
+
+        lastError = 0
+        self.onConnect()
+
+        while True:
+            try:
+                self.execution.processCommandQueue()
+            except Exception as e:
+                self.log.log_exception(e)
+                if time.time() - lastError < 10:
+                    self.log.log("Program keeps crashing, please restart")
+                    break
+                self.lastError = time.time()
+                if self.irc.connected == False:
+                    self.errorOut("IRC Disconnected")
+                    self.onDisconnect()
+                    return
+
+        self.errorOut("Quitting")
+
+    def stop(self):
+        self.irc.close()
