dc73c23057ab9d31620609916b459466cc9263f5
Joe Pentland <joe.pentland@gmail.com>
2021-07-15 14:59:33 +0200

    config: Migrate old config directory

--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@ import toml
 import pathlib
 from appdirs import *
 from error import *
+from shutil import copytree, rmtree
 
 defaultConfig = {
         "execution" : {
@@ -32,6 +33,7 @@ class Config:
         self.commands_file = "commands.toml"
         self.user_commands_file = "usercommands.toml"
         self.defaultconf_dir = "defaultconf"
+        self.migrateOldConfig()
         self.data = self.loadConfig()
 
     # Get contents of config file or defaultconfig if doesnt exist
@@ -58,6 +60,17 @@ class Config:
 
         except FileNotFoundError:
             return defaultConfig
+
+    # Migrate old config files from before rename
+    def migrateOldConfig(self):
+        OLD_APPNAME = "TwitchPlaysRTS"
+        OLD_APPAUTHOR = "TwitchPlaysRTS"
+        OLD_PATH = user_data_dir(OLD_APPNAME, OLD_APPAUTHOR)
+
+        if os.path.isdir(OLD_PATH) and not os.path.isdir(self.config_dir):
+                pathlib.Path(os.path.dirname(self.config_dir)).mkdir(parents=True, exist_ok=True)
+                copytree(OLD_PATH, self.config_dir)
+                rmtree(OLD_PATH)
 
     # Write config to disk
     def write(self):
