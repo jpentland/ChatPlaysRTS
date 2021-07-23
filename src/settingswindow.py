67e18fd7afc1455dc144407e4fe2a3b4e08d5593
Joe Pentland <joe.pentland@gmail.com>
2021-07-23 10:49:28 +0200

    settingswindow: Arrange into tabs

--- src/settingswindow.py
+++ src/settingswindow.py
@@ -10,27 +10,37 @@ class Settingswindow(tk.Toplevel):
         self.log = log
         self.vars = {}
         self.descriptions = config.getDescriptions()
-        self.settingsFrame = tk.Frame(self, relief = tk.GROOVE, borderwidth = 1)
-        self.buildSettings(self.settingsFrame)
-        # add apply button
+        self.notebook = ttk.Notebook(self)
+        self.buildSettings(self.notebook)
         self.applyFrame = tk.Frame(self, padx = 5, pady = 5)
+        self.okButton = tk.Button(self.applyFrame, text = "Ok", command = self.ok)
         self.applyButton = tk.Button(self.applyFrame, text = "Apply", command = self.apply)
         self.cancelButton = tk.Button(self.applyFrame, text = "Cancel", command = self.cancel)
+        self.defaultButton = tk.Button(self.applyFrame, text = "Reset to Default", command = self.default)
+        self.okButton.pack(side = tk.RIGHT)
         self.applyButton.pack(side = tk.RIGHT)
         self.cancelButton.pack(side = tk.RIGHT)
-        self.settingsFrame.pack(anchor = tk.W, fill = tk.X, padx = 5, pady = 5)
-        self.applyFrame.pack(fill = tk.X)
+        self.defaultButton.pack(side = tk.LEFT)
+        self.notebook.pack(anchor = tk.W, fill = tk.X, padx = 5, pady = 5)
+        self.applyFrame.pack(fill = tk.BOTH)
 
-    def buildSettings(self, frame):
+    def buildSettings(self, notebook):
         self.gridSize = 0
         for section in self.config.data:
             self.vars[section] = {}
-            self.buildSection(section, frame)
+            self.buildSection(section, notebook)
 
-    def buildSection(self, section, frame):
-        # Add section header
+    def buildSection(self, section, notebook):
+        if section not in self.descriptions:
+            return
+
+        sectionFrame = ttk.Frame(notebook, relief = tk.GROOVE, borderwidth = 1)
+        sectionFrame.gridSize = 0
+
         for setting in self.config[section]:
-            self.buildSetting(section, setting, frame)
+            self.buildSetting(section, setting, sectionFrame)
+
+        notebook.add(sectionFrame, text = section)
 
     def buildSetting(self, section, setting, frame):
         try:
@@ -38,21 +48,28 @@ class Settingswindow(tk.Toplevel):
         except KeyError:
             return
 
-        self.gridSize += 2
+        frame.gridSize += 2
         label = tk.Label(frame, text = setting, font = ('Arial', 10, 'bold'), justify = tk.LEFT)
         self.vars[section][setting] = tk.StringVar(self)
         self.vars[section][setting].set(self.config[section][setting])
         entry = tk.Entry(frame, textvariable = self.vars[section][setting])
         descriptionLabel = tk.Label(frame, text = description, wraplength = 400, justify = tk.LEFT, font = ('Arial', 10, 'italic'))
-        if self.gridSize != 2:
+        if frame.gridSize != 2:
             sep = ttk.Separator(frame, orient = 'horizontal')
-            sep.grid(row = self.gridSize - 1, column = 1, columnspan = 3, sticky = "ew")
-        label.grid(row = self.gridSize, column = 1, sticky = tk.E, padx = 5, pady =5)
-        entry.grid(row = self.gridSize, column = 2, sticky = tk.W, pady = 5)
-        descriptionLabel.grid(row = self.gridSize, column = 3, sticky = tk.W, padx = 5, pady = 5)
+            sep.grid(row = frame.gridSize - 1, column = 1, columnspan = 3, sticky = "ew")
+        label.grid(row = frame.gridSize, column = 1, sticky = tk.E, padx = 5, pady =5)
+        entry.grid(row = frame.gridSize, column = 2, sticky = tk.W, pady = 5)
+        descriptionLabel.grid(row = frame.gridSize, column = 3, sticky = tk.W, padx = 5, pady = 5)
 
     def cancel(self):
-        pass
+        self.destroy()
 
     def apply(self):
-        pass
+        self.log.log("Updating config not implemented")
+
+    def ok(self):
+        self.apply()
+        self.cancel()
+
+    def default(self):
+        self.log.log("Reset default not implemented")
