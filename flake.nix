4be6e353805fb705af2aa90b109db7838da99ba9
Joe Pentland <joe.pentland@gmail.com>
2024-02-23 11:16:22 +0100

    flake.nix, flake.lock: Allow devshell for running

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Python development environment with specific packages";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        python = pkgs.python311;
+        python_packages = python.withPackages (ps: with ps; [
+          pyautogui
+          appdirs
+          toml
+          screeninfo
+        ]);
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ python_packages ];
+        };
+      }
+    );
+}
